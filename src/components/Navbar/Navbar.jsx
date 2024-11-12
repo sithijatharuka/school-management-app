eb522486c18028ffb0e75a5d4b172db3f06e0ee5
sithijatharuka <sithijatharuka03@gmail.com>
2024-11-12 09:12:29 +0530

    make menu mobile mobile responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,9 @@
 
 import styles from "@/components/Navbar/Navbar.module.css";
 import { UserButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { useState } from "react";
+import Menu from "../Menu/Menu";
 
 export default function Navbar() {
   const { isLoaded, isSignedIn, user } = useUser();
@@ -9,15 +12,31 @@ export default function Navbar() {
   const role = user?.publicMetadata.role;
   const name = user?.fullName;
 
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className={styles.container}>
+      <div className={styles.menuBox}>
+        <button onClick={toggleMenu} className={styles.menuBtn}>
+          <Image src="/menu.png" alt="" width={20} height={20} />
+        </button>
+      </div>
       <div className={styles.nameBox}>
-        <h3>{name}</h3>
-        <h5>{role}</h5>
+        <div className={styles.names}>
+          <h3>{name}</h3>
+          <h5>{role}</h5>
+        </div>
+        <div className={styles.avatarContainer}>
+          <UserButton />
+        </div>
       </div>
-      <div className={styles.avatarContainer}>
-        <UserButton />
-      </div>
+
+      {isMenuOpen && ( // Conditionally render the menu when it's open
+        <Menu />
+      )}
     </div>
   );
 }
